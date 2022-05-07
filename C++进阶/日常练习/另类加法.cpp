59d9b8268219819459d50c64ba1318a9b1fc3611
流年玄月 <984014088@qq.com>
2022-05-07 22:43:17 +0800

    每日一题

---
+++ C++进阶/日常练习/另类加法.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+/*
+
+不用加减乘除做加法
+链接：https://www.nowcoder.com/questionTerminal/59ac416b4b944300b617d4f7f111b215
+来源：牛客网
+
+写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
+
+数据范围：两个数都满足 -10 \le n \le 1000−10≤n≤1000
+
+*/
+
+class Solution {
+public:
+    int Add(int num1, int num2) {
+        //进位数
+        int up = (num1 & num2) << 1;
+        int at = num1 ^ num2;
+        return at + up;
+    }
+};
