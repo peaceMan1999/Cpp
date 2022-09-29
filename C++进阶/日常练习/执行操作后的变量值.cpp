4a9c762ef7ed13247f9f87e653561cbb6f7b6031
流年玄月 <984014088@qq.com>
2022-09-29 23:13:59 +0800

    力扣练习

---
+++ C++进阶/日常练习/执行操作后的变量值.cpp
@@ -0,0 +1,45 @@
+﻿/*
+
+存在一种仅支持 4 种操作和 1 个变量 X 的编程语言：
+
+++X 和 X++ 使变量 X 的值 加 1
+--X 和 X-- 使变量 X 的值 减 1
+最初，X 的值是 0
+
+给你一个字符串数组 operations ，这是由操作组成的一个列表，返回执行所有操作后， X 的 最终值 。
+
+ 
+
+示例 1：
+
+输入：operations = ["--X","X++","X++"]
+输出：1
+解释：操作按下述步骤执行：
+最初，X = 0
+--X：X 减 1 ，X =  0 - 1 = -1
+X++：X 加 1 ，X = -1 + 1 =  0
+X++：X 加 1 ，X =  0 + 1 =  1
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/final-value-of-variable-after-performing-operations
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (int i = 0; i < operations.size(); ++i)
+        {
+            if (operations[i][1] == '+')
+                ++x;
+            else if (operations[i][1] == '-')
+                --x;
+        }
+        return x;
+    }
+};
+
+
+
