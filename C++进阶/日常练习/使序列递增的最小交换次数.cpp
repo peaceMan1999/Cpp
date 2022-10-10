344133f82ce3c68dfe9727b666546e73a6f0b3de
流年玄月 <984014088@qq.com>
2022-10-10 23:56:24 +0800

    贪心算法

---
+++ C++进阶/日常练习/使序列递增的最小交换次数.cpp
@@ -0,0 +1,37 @@
+﻿/*
+我们有两个长度相等且不为空的整型数组 nums1 和 nums2 。在一次操作中，我们可以交换 nums1[i] 和 nums2[i]的元素。
+
+例如，如果 nums1 = [1,2,3,8] ， nums2 =[5,6,7,4] ，你可以交换 i = 3 处的元素，得到 nums1 =[1,2,3,4] 和 nums2 =[5,6,7,8] 。
+返回 使 nums1 和 nums2 严格递增 所需操作的最小次数 。
+
+数组 arr 严格递增 且  arr[0] < arr[1] < arr[2] < ... < arr[arr.length - 1] 。
+
+注意：
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/minimum-swaps-to-make-sequences-increasing
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        int a = 0, b = 1, n = nums1.size();
+        for (int i = 1; i < n; ++i) {
+            int x = a, y = b;
+            if (nums1[i - 1] >= nums1[i] || nums2[i - 1] >= nums2[i]) {
+                a = y, b = x + 1;
+            }
+            else {
+                b = y + 1;
+                if (nums1[i - 1] < nums2[i] && nums2[i - 1] < nums1[i]) {
+                    a = min(a, y);
+                    b = min(b, x + 1);
+                }
+            }
+        }
+        return min(a, b);
+    }
+};
+
+
