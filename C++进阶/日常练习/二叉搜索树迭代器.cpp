d4a595a2dbb93b1ac7c8defb6e74e5ecc6e73ba1
流年玄月 <984014088@qq.com>
2022-10-23 21:46:49 +0800

    力扣

---
+++ C++进阶/日常练习/二叉搜索树迭代器.cpp
@@ -0,0 +1,86 @@
+﻿/*
+现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+
+BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+int next()将指针向右移动，然后返回指针处的数字。
+注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+
+可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/kTOapQ
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+输入
+inputs = ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+inputs = [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+输出
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+解释
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // 返回 3
+bSTIterator.next();    // 返回 7
+bSTIterator.hasNext(); // 返回 True
+bSTIterator.next();    // 返回 9
+bSTIterator.hasNext(); // 返回 True
+bSTIterator.next();    // 返回 15
+bSTIterator.hasNext(); // 返回 True
+bSTIterator.next();    // 返回 20
+bSTIterator.hasNext(); // 返回 False
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/kTOapQ
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+    vector<int> vv;
+    int n;
+public:
+    void dfs(TreeNode* root)
+    {
+        if (root == nullptr)
+            return;
+
+        dfs(root->left);
+        vv.push_back(root->val);
+        dfs(root->right);
+    }
+
+    BSTIterator(TreeNode* root) {
+        dfs(root);
+        // 因为第一个是0
+        n = -1;
+    }
+
+    int next() {
+        return vv[++n];
+    }
+
+    bool hasNext() {
+        // 因为size()返回值是unsigned_int类型，所以要转为int
+        return n < (int)vv.size() - 1;
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
