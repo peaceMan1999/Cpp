a83181a53033d10d60c34cea26405f314f046ac0
流年玄月 <984014088@qq.com>
2022-11-07 23:20:36 +0800

    力扣dp

---
+++ C++进阶/dp/最长递增子序列.cpp
@@ -0,0 +1,23 @@
+/* 
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/longest-increasing-subsequence
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
